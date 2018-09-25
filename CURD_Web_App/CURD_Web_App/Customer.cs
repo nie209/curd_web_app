643c0cd561c19f5dc6a0cf335664a5291fdb70b1
Wilson Nie <wilson.nie13@gmail.com>
2018-09-24 20:05:53 -0700

    Create page has been added, setting up inline database completeonce you create a customer it will redirect you back to index page

---
+++ CURD_Web_App/CURD_Web_App/Customer.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CURD_Web_App
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        [Required, StringLength(20)]
+        public string Name { get; set; }
+    }
+}
--- CURD_Web_App/CURD_Web_App/Startup.cs
+++ CURD_Web_App/CURD_Web_App/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 
 namespace CURD_Web_App
 {
@@ -33,6 +34,7 @@ namespace CURD_Web_App
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("myDB"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
